52377c651be9d0e7708095fc81e16d3d12883554
krist <krist@LAPTOP-HIQLHLL6>
2022-11-15 22:53:48 +0500

    Добавьте файлы проекта.

---
+++ Lab 8.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp";
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
